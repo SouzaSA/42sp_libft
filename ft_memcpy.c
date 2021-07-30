4c2226799755e28ecee857a4878780e9d0ac7bda
Silvio De alvarenga souza <sde-alva@c1r3p3.42sp.org.br>
2021-07-30 00:25:25 -0300

    added some protections

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/23 14:40:59 by sde-alva          #+#    #+#             */
-/*   Updated: 2021/07/25 17:25:51 by sde-alva         ###   ########.fr       */
+/*   Updated: 2021/07/30 00:00:08 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n)
+	while (i < n && (dest || src))
 	{
 		((char *)dest)[i] = ((char *)src)[i];
 		i++;
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/23 14:48:00 by sde-alva          #+#    #+#             */
-/*   Updated: 2021/07/28 15:32:21 by sde-alva         ###   ########.fr       */
+/*   Updated: 2021/07/30 00:02:54 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,19 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	size_t		i;
 
 	i = 0;
-	if (dest < src)
+	if (dest || src)
 	{
-		ft_memcpy(dest, src, n);
-	}
-	else
-	{
-		while (i < n)
+		if (dest < src)
 		{
-			((char *)dest)[n - i - 1] = ((char *)src)[n - i - 1];
-			i++;
+			ft_memcpy(dest, src, n);
+		}
+		else
+		{
+			while (i < n)
+			{
+				((char *)dest)[n - i - 1] = ((char *)src)[n - i - 1];
+				i++;
+			}
 		}
 	}
 	return (dest);
